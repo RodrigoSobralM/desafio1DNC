064b363d8f4f363b6cfcbbee140963e5b53d5ebf
RodrigoSobralM <rodrigo_sobral.m@hotmai.com>
2024-07-10 20:21:04 -0300

    :sparkles: feat: Adicionado uma nova tag para mostrar uma mensagem ao usuario após o envio do fomulario, lógica aplicada no form.js tambem.

--- form.js
+++ form.js
@@ -1,19 +1,27 @@
 const handleSubmit = (event) => {
-    event.preventDefault()
+  event.preventDefault();
 
-    const nome = document.querySelector('input[name=Nome]').value
-    const email = document.querySelector('input[name=Email]').value
+  const nome = document.querySelector("input[name=Nome]").value;
+  const email = document.querySelector("input[name=Email]").value;
 
-    fetch('https://api.sheetmonkey.io/form/wX4ZozsizATSn7mkuuVzfx', {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({nome: nome, email: email})
-    })
-}
+  fetch("https://api.sheetmonkey.io/form/wX4ZozsizATSn7mkuuVzfx", {
+    method: "post",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nome: nome, email: email }),
+  }).then((response) => {
+    if (response.ok) {
+      document.querySelector("#formulario").reset();
+      document.querySelector("#form-message").textContent = "Muito obrigado, entraremos em contato em breve!";
+      document.querySelector("#form-message").style.color = "green";
+    }
+  });
+};
 
-
-
-document.querySelector('form').addEventListener('submit', handleSubmit)
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector("#formulario")
+    .addEventListener("submit", handleSubmit);
+});
