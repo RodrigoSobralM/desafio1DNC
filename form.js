4fd28791f821ad5a48fea0b6acffb0ee6f9a3d18
RodrigoSobralM <rodrigo_sobral.m@hotmai.com>
2024-07-10 19:56:12 -0300

    :recycle: refactor: Modificando chamada de api para o arquivo form.js

--- form.js
+++ form.js
@@ -1,32 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("formulario");
-    const formMessage = document.getElementById("form-message");
+const handleSubmit = (event) => {
+    event.preventDefault()
 
-    form.addEventListener("submit", async (event) => {
-        event.preventDefault();
+    const nome = document.querySelector('input[name=Nome]').value
+    const email = document.querySelector('input[name=Email]').value
 
-        const formData = new FormData(form);
-        const formObject = Object.fromEntries(formData.entries());
+    fetch('https://api.sheetmonkey.io/form/7KtQWF3Xyk8Eq9NEX4x9kf', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({nome: nome, email: email})
+    })
+}
 
-        try {
-            const response = await fetch("https://api.sheetmonkey.io/form/7KtQWF3Xyk8Eq9NEX4x9kf", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(formObject),
-            });
 
-            if (response.ok) {
-                formMessage.textContent = "Obrigado por entrar em contato!";
-                formMessage.style.color = "green";
-                form.reset();
-            } else {
-                throw new Error("Erro ao enviar o formulário. Tente novamente mais tarde.");
-            }
-        } catch (error) {
-            formMessage.textContent = error.message;
-            formMessage.style.color = "red";
-        }
-    });
-});
+
+document.querySelector('form').addEventListener('submit', handleSubmit)
